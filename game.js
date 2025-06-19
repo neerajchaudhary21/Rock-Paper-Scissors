8d2e75b2163087c441005f3baa92cc01e4cde110
neerajchaudhary21 <cneeraj872@gmail.com>
2025-06-19 23:43:01 +0530

    Add buttons to play

--- game.js
+++ game.js
@@ -1,71 +1,116 @@
+const rockbutton = document.getElementById("rock");
+const paperbutton = document.getElementById("paper");
+const scissorbutton = document.getElementById("scissors");
+const humanpick = document.getElementById("humanchoice");
+const comppick = document.getElementById("compchoice");
+const submit = document.getElementById("submit");
+const humanscore = document.getElementById("humanscore");
+const compscore = document.getElementById("compscore");
+const roundresult = document.getElementById("roundresult");
+const reset = document.getElementById("reset");
+const ending = document.getElementById("ending");
+let temp;
+let totalhumanscore = 0;
+let totalcomputerscore = 0;
+
 const getcomputerchoice = function(){
   let value = Math.floor(Math.random()*3);
-  if(value==1) return "rock";
-  else if(value==2) return "paper";
-  else return "scissors";
-}
-
-
-const gethumanchoice = function(chosenvalue){
-   if(chosenvalue==1) return "rock";
-   else if(chosenvalue==2) return "paper";
-   else return "scissors";
+  if(value==1) return "Rock";
+  else if(value==2) return "Paper";
+  else return "Scissor";
 }
 
 function playRound(hs,cs){
    let hscore = 0;
    let cscore = 0;
    if(hs===cs) {
-    console.log("Draw");
+    roundresult.textContent = "Draw";
     return [0,0];
    }
-   if(hs=="rock" && cs=="paper"){
+   if(hs=="Rock" && cs=="Paper"){
       cscore += 1;
-       console.log(`You lose,${cs} beats ${hs}`);
+      roundresult.textContent = `You lose,${cs} beats ${hs}`;
    }
-   else if(hs=="rock" && cs=="scissors"){
+   else if(hs=="Rock" && cs=="Scissor"){
       hscore += 1;
-      console.log(`You win,${hs} beats ${cs}`);
+      roundresult.textContent =`You win,${hs} beats ${cs}`;
    }
-   else if(hs=="paper" && cs=="rock"){
+   else if(hs=="Paper" && cs=="Rock"){
      hscore += 1;
-    console.log(`You win,${hs} beats ${cs}`);
+    roundresult.textContent = `You win,${hs} beats ${cs}`;
    }
-   else if(hs=="paper" && cs=="scissors"){
+   else if(hs=="Paper" && cs=="Scissors"){
      cscore += 1;
-      console.log(`You lose,${cs} beats ${hs}`);
+      roundresult.textContent = `You lose,${cs} beats ${hs}`;
    }
-   else if(hs=="scissors" && cs=="rock"){
+   else if(hs=="Scissor" && cs=="Rock"){
      cscore += 1;
-     console.log(`You lose,${cs} beats ${hs}`);
+     roundresult.textContent = `You lose,${cs} beats ${hs}`;
    }
-   else if(hs=="scissors" && cs=="paper"){
+   else if(hs=="Scissors" && cs=="Paper"){
       hscore += 1;
-      console.log(`You win,${hs} beats ${cs}`);
+      roundresult.textContent = `You win,${hs} beats ${cs}`;
    } 
    return [hscore,cscore];
 }
 
 
-let humanscore = 0;
-let computerscore = 0;
-const computerselection = getcomputerchoice();
-
-
-for(let i = 0;i<5;i++){
-  let chosenvalue = parseInt(prompt("Enter the value,1 for rock,2 for paper,3 for scissors"));
-  const humanselection = gethumanchoice(chosenvalue);
-  const [hscore,cscore] = playRound(humanselection,computerselection);
-   humanscore += hscore;
-   computerscore += cscore;
-   if(humanscore>=3) {
-    console.log("You Win!!");
-     break;
-   }
-   if(computerscore>=3) {
-    console.log("You Lose!!");
-    break;
-   }
+function playagain(){
+    totalhumanscore = 0;
+   totalcomputerscore = 0;
+   humanscore.textContent = totalhumanscore;
+   compscore.textContent = totalcomputerscore;
+   roundresult.textContent = "";
 }
 
-console.log("Match has been drawn");
+
+
+
+rockbutton.addEventListener("click",function(){
+   humanpick.textContent = "Rock";
+   temp = "Rock";
+   return temp;
+})
+
+paperbutton.addEventListener("click",function(){
+   humanpick.textContent = "Paper";
+   temp = "Paper";
+   return temp;
+})
+
+scissorbutton.addEventListener("click",function(){
+   humanpick.textContent = "Scissor";
+   temp = "Scissor";
+   return temp;
+})
+
+submit.addEventListener("click",function(){
+   const res = getcomputerchoice();
+   comppick.textContent = `${res}`;
+   const[hs,cs] =  playRound(temp,res);
+   totalhumanscore += hs;
+   totalcomputerscore += cs;
+   humanscore.textContent = totalhumanscore;
+   compscore.textContent = totalcomputerscore;
+})
+
+reset.addEventListener("click",function(){
+   playagain();
+});
+
+ending.addEventListener("click",function(){
+   if(totalhumanscore>totalcomputerscore){
+     roundresult.textContent = "Congratulations,You are the champion";
+   }
+   else if(totalhumanscore<totalcomputerscore){
+    roundresult.textContent = "You lose,Better luck next time";
+   }
+   else{
+    roundresult.textContent = "Tough fight Champ,it's a draw";
+   }
+   setTimeout(playagain,3000);
+})
+
+
+
+
